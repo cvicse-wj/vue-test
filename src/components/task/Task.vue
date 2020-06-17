ec0dacd373cfea81fe061f9a5da1b586a38097ad
wj_glb <wj_glb@hj.com>
2020-06-17 20:54:40 +0800

    是是是

---
+++ src/components/task/Task.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<TaskHead></TaskHead>
+		<TaskList></TaskList>
+		<TaskFooter></TaskFooter>
+	</div>
+</template>
+
+<script>
+	import TaskHead from '@/components/task/TodeTaskHead.vue'
+	import TaskList from '@/components/task/TodeList.vue'
+	import TaskFooter from '@/components/task/TaskFooter.vue'
+	export default{
+		name:'Task',
+		data(){
+			return{
+				
+			}
+		},
+		components:{
+			TaskHead,
+			TaskList,
+			TaskFooter
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ src/components/task/TodoHead.vue
@@ -0,0 +1,25 @@
+<template>
+	<input placeholder="请输入你的任务名称,按enter键进行确认提交!" @keyup.enter="toSubmit" v-model="todo"/>
+</template>
+
+<script>
+	export default{
+		name:'TodoHead',
+		data(){
+			return{
+				todo:""
+			}
+		},
+		props:{
+			todos:Array
+		},
+		methods:{
+			toSubmit() {
+				this.todos.push(this.todo)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
